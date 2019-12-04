b1ee6ef60cb03e83a43160f97db0550da6c6d169
DciAlex <alexgherman68@gmail.com>
2019-12-04 15:48:35 +0100

    update 0,1

--- script.js
+++ script.js
@@ -13,14 +13,29 @@ var antwortFeld = document.getElementById('antwort')
 let frage;
 
 neueAufgabeButton.onclick = (e)=> {
+  new1()
+}
+function new1 (){
   frage = questions[Math.floor(Math.random()*questions.length)];
   frageDiv.innerText = frage.question
+new Promise((resolve,reject) =>  {
+    setTimeout(function() {
+      
+      if ( antwortFeld.value.trim() === frage.answer ){
+        resolve(punkteDiv.innerText = Number(punkteDiv.innerText) + 100)
+        
+      } else {
+        reject(punkteDiv.innerText = Number(punkteDiv.innerText) - 100)
+      }
+      new1()
+    }, 3000)
+  })
 }
 
-antwortButton.onclick = (e)=> {
-  if ( antwortFeld.value.trim() === frage.answer ){
-    punkteDiv.innerText = Number(punkteDiv.innerText) + 100
-  } else {
-    punkteDiv.innerText = Number(punkteDiv.innerText) - 100
-  }
-}
+// antwortButton.onclick = (e)=> {
+//   if ( antwortFeld.value.trim() === frage.answer ){
+//     punkteDiv.innerText = Number(punkteDiv.innerText) + 100
+//   } else {
+//     punkteDiv.innerText = Number(punkteDiv.innerText) - 100
+//   }
+// }
